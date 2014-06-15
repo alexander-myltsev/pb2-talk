2984f988bbccf90f19fa6b6d06bc620ca7259d0e
Alexander Myltsev <alexander.myltsev@phystech.edu>
2014-06-15 16:16:18 +0200

    Add my theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,6 +41,7 @@ module.exports = function(grunt) {
 			main: {
 				files: {
 					'css/theme/default.css': 'css/theme/source/default.scss',
+                    'css/theme/my.css': 'css/theme/source/my.scss',
 					'css/theme/beige.css': 'css/theme/source/beige.scss',
 					'css/theme/night.css': 'css/theme/source/night.scss',
 					'css/theme/serif.css': 'css/theme/source/serif.scss',
